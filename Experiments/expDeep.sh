8d2f7c760d8ee7cca075a3cfd42eca4d8859c3fd
Fan Wu <fan.wu.12@ucl.ac.uk>
2016-04-16 16:38:46 +0100

    add deep related experiments script

---
+++ Experiments/expDeep.sh
@@ -0,0 +1,24 @@
+if [ $# -lt 1 ]; then
+        echo "Invoke script followed with subject name"
+        exit 2
+fi
+
+subject=$1
+
+cp -r {$subject}_population_for_deep chamber/population
+cd chamber
+cp libmalloc.so libmalloc.so.bkp
+
+cp ../{$subject}_malloc/libmalloc.so0 libmalloc.so
+./evalAll.sh $subject
+cp population/newProperty.txt property.0
+
+cp ../{$subject}_malloc/libmalloc.so1 libmalloc.so
+./evalAll.sh $subject
+cp population/newProperty.txt property.1
+
+cp ../{$subject}_malloc/libmalloc.so0 libmalloc.so
+./evalAll.sh $subject
+cp population/newProperty.txt property.2
+
+cp libmalloc.so.bkp libmalloc.so
